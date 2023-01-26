765a63adfd3375d74c9348b818d6bd4fcb23d5af
SoYoung Kim <sohappy1220@gmail.com>
2023-01-26 20:36:47 +0900

    test: notion db 연결

---
+++ src/pages/api/add-item.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from '@notionhq/client'
+
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+console.log(databaseId)
+
+async function addItem(name: string) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+      },
+    })
+    console.log(response)
+    console.log('Success! Entry added.')
+  } catch (error) {
+    console.error(JSON.stringify(error))
+  }
+}
+
+type Data = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { name } = req.query
+  if (name === null) {
+    res.status(400).json({ message: `No name` })
+  }
+  try {
+    await addItem(String(name))
+    res.status(200).json({ message: `Success ${name}` })
+  } catch (error) {
+    res.status(400).json({ message: `Failed ${name}` })
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useRef } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const handleClick = () => {
+    if (inputRef.current == null || inputRef.current.value == '') {
+      alert('name을 넣어주세요.')
+      return
+    }
+    fetch(`/api/add-item?name=${inputRef.current.value}`)
+      .then((res) => res.json())
+      .then((data) => alert(data.message))
+  }
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
     <>
       <Head>
@@ -37,6 +48,8 @@ export default function Home() {
               />
             </a>
           </div>
+          <input ref={inputRef} type="text" placeholder="name" />
+          <button onClick={handleClick}>Add Jacket</button>
         </div>
 
         <div className={styles.center}>
